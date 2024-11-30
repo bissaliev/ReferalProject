319f1b4349d154858d5e86f8517521ee43a061ca
bissaliev <bissaliev21@gmail.com>
2024-11-30 22:06:47 +0400

    users: Добавил функцию generate_invite_code для генерации инвайт-кода

--- referralapp/users/utils.py
+++ referralapp/users/utils.py
@@ -1,6 +1,11 @@
-from random import randint
+from random import choices, randint
 
 
 def generate_confirm_code() -> str:
-    """Генерация кода авторизации."""
+    """Генерация 4-х значного кода авторизации."""
     return str(randint(1000, 9999))
+
+
+def generate_invite_code(length: int = 6) -> str:
+    """Генерация инвайт-кода."""
+    return "".join(choices("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=length))
