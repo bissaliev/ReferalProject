84bf10b8856869d509482b89104419c3d3ba2f64
bissaliev <bissaliev21@gmail.com>
2024-11-30 22:51:35 +0400

    api: Добавил правильную обработки ошибок при получение токена

--- referralapp/api/views.py
+++ referralapp/api/views.py
@@ -6,6 +6,7 @@ from api.serializers import (
 )
 from api.utils import send_confirmation_code
 from django.contrib.auth import get_user_model
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -45,12 +46,16 @@ class CodeVerificationView(APIView):
         phone_number = serializer.validated_data.get("phone_number")
         confirmation_code = serializer.validated_data.get("confirmation_code")
         try:
-            user = User.objects.get(
-                phone_number=phone_number, confirmation_code=confirmation_code
-            )
+            user = User.objects.get(phone_number=phone_number)
         except User.DoesNotExist:
-            return Response({"error": "Invalid code"})
+            return Response(
+                {"error": "Неверный или не существующий номер телефона."}
+            )
+        if user.confirmation_code != confirmation_code:
+            return Response({"error": "Неверный код."})
         token, _ = Token.objects.get_or_create(user=user)
+        user.confirmation_code = None
+        user.save(update_fields=["confirmation_code"])
         return Response({"token": token.key})
 
 
@@ -78,13 +83,24 @@ class UserViewSet(ModelViewSet):
         """Активация инвайт-кода."""
         current_user = request.user
         if current_user.inviter:
-            return Response({"error": "Инвайт код уже активирован"})
+            return Response(
+                {"error": "Инвайт код уже активирован"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         serializer = InviteCodeSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         invite_code = serializer.validated_data.get("invite_code")
+        if invite_code == current_user.invite_code:
+            return Response(
+                {"error": "Вы не можете пригласить самого себя."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         inviter = User.objects.filter(invite_code=invite_code).first()
         if not inviter:
-            return Response({"error": "Указанный инвайт-код не существует."})
+            return Response(
+                {"error": "Указанный инвайт-код не существует."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         current_user.inviter = inviter
         current_user.save(update_fields=["inviter"])
         return Response({"message": "Инвайт-код успешно активирован."})
