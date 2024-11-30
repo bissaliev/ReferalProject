7ae8f36d89bfb5213aed77d905e8aa9c31660f7f
bissaliev <bissaliev21@gmail.com>
2024-11-30 17:00:01 +0400

    Добавил представления для аутентификации

--- referralapp/api/views.py
+++ referralapp/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from api.serializers import AuthTokenSerializer, PhoneSerializer
+from api.utils import send_confirmation_code
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+User = get_user_model()
+
+
+class PhoneAuthView(APIView):
+    serializer_class = PhoneSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        phone_number = serializer.validated_data.get("phone_number")
+        user, _ = User.objects.get_or_create(phone_number=phone_number)
+        new_code = user.generate_confirmation_code()
+        send_confirmation_code(phone_number, new_code)
+        return Response(
+            {"message": f"Код отправлен на номер {phone_number}"}, status=200
+        )
+
+
+class CodeVerificationView(APIView):
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        phone_number = serializer.validated_data.get("phone_number")
+        confirmation_code = serializer.validated_data.get("confirmation_code")
+        try:
+            user = User.objects.get(
+                phone_number=phone_number, confirmation_code=confirmation_code
+            )
+        except User.DoesNotExist:
+            return Response({"error": "Invalid code"})
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
