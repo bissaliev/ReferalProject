af6e4d6c002de9b2fe899baed5f41d5e7f320d48
bissaliev <bissaliev21@gmail.com>
2024-11-30 22:11:08 +0400

    api: Добавил сериализаторы UserSerializer и InviteCodeSerializer

--- referralapp/api/serializers.py
+++ referralapp/api/serializers.py
@@ -1,12 +1,18 @@
 import re
 
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()
 
 
 class PhoneSerializer(serializers.Serializer):
-    phone_number = serializers.CharField(max_length=15)
+    """Сериализатор для авторизации пользователя."""
+
+    phone_number = serializers.CharField(required=True)
 
     def validate_phone_number(self, value):
+        """Валидация номера телефона."""
         if not re.match(r"^\+?\d{10,15}$", value):
             raise serializers.ValidationError(
                 "Введите корректный номер телефона."
@@ -15,4 +21,40 @@ class PhoneSerializer(serializers.Serializer):
 
 
 class AuthTokenSerializer(PhoneSerializer):
-    confirmation_code = serializers.CharField(max_length=4)
+    """
+    Сериализатор для получения токена по номеру телефона и коду верификации.
+    """
+
+    confirmation_code = serializers.CharField(max_length=4, required=True)
+
+
+class InviteCodeSerializer(serializers.Serializer):
+    """Сериализатор для активации инвайт-кода."""
+
+    invite_code = serializers.CharField(required=True)
+
+    def validate_invite_code(self, value):
+        """Валидация инвайт-кода."""
+        if not re.match(r"^[a-zA-Z0-9]{6}$", value):
+            raise serializers.ValidationError("Некорректный инвайт код")
+        return value
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор пользователя."""
+
+    inviters = serializers.SlugRelatedField(
+        slug_field="phone_number", many=True, read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "phone_number",
+            "username",
+            "first_name",
+            "last_name",
+            "invite_code",
+            "inviters",
+        )
