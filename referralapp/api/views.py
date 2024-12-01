3715343208775b2d04cc59a69ba937d83c201195
bissaliev <bissaliev21@gmail.com>
2024-12-01 09:32:55 +0400

    Документация. Первоначальные определения

--- referralapp/api/views.py
+++ referralapp/api/views.py
@@ -1,11 +1,18 @@
 from api.serializers import (
     AuthTokenSerializer,
+    DummyDetailAndStatusSerializer,
     InviteCodeSerializer,
     PhoneSerializer,
     UserSerializer,
+    ErrorResponseSerializer,
 )
 from api.utils import send_confirmation_code
 from django.contrib.auth import get_user_model
+from drf_spectacular.utils import (
+    OpenApiExample,
+    OpenApiResponse,
+    extend_schema,
+)
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
@@ -16,11 +23,34 @@ from rest_framework.viewsets import ModelViewSet
 User = get_user_model()
 
 
+@extend_schema(tags=["Аутентификация"])
 class PhoneAuthView(APIView):
-    """Представление для запроса код верификации для авторизации."""
+    """Вход/регистрация по номеру телефона."""
 
     serializer_class = PhoneSerializer
 
+    @extend_schema(
+        operation_id="Вход или регистрация пользователя.",
+        request=PhoneSerializer,
+        responses={
+            200: OpenApiResponse(
+                response=DummyDetailAndStatusSerializer,
+                description="Код успешно отправлен на указанный номер телефона.",
+                examples=[
+                    OpenApiExample(
+                        name="message",
+                        value={
+                            "message": "Код отправлен на номер +1234567890"
+                        },
+                    )
+                ],
+            ),
+        },
+        description=(
+            "Пользователь указывает номер телефона на который будет "
+            "отправлен код верификации."
+        ),
+    )
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -31,15 +61,64 @@ class PhoneAuthView(APIView):
         # отправка кода верификации пользователю
         send_confirmation_code(phone_number, new_code)
         return Response(
-            {"message": f"Код отправлен на номер {phone_number}"}, status=200
+            {"message": f"Код отправлен на номер {phone_number}"},
+            status=status.HTTP_200_OK,
         )
 
 
+@extend_schema(tags=["Аутентификация"])
 class CodeVerificationView(APIView):
     """Представление верифицирует по номеру телефона и коду верификации."""
 
     serializer_class = AuthTokenSerializer
 
+    @extend_schema(
+        operation_id="Получение токена по номеру телефона и коду верификации",
+        request=AuthTokenSerializer,
+        responses={
+            200: OpenApiResponse(
+                response={
+                    "type": "object",
+                    "properties": {"token": {"type": "string"}},
+                },
+                description="Успешная верификация кода. Возвращается токен пользователя.",
+                examples=[
+                    OpenApiExample(
+                        name="token",
+                        value={
+                            "token": "9754d29331447ec35a23e9141c6b48ec7309d141"
+                        },
+                    )
+                ],
+            ),
+            400: OpenApiResponse(
+                response=ErrorResponseSerializer,
+                description="Ошибки в процессе верификации. Например, неверный код или номер телефона.",
+                examples=[
+                    OpenApiExample(
+                        name="error 400",
+                        value={"error": "Неверный код верификации."},
+                    )
+                ],
+            ),
+            404: OpenApiResponse(
+                response={
+                    "type": "object",
+                    "properties": {"error": {"type": "string"}},
+                },
+                description="Номер телефона не найден.",
+                examples=[
+                    OpenApiExample(
+                        name="error 404",
+                        value={
+                            "error": "Неверный или не существующий номер телефона"
+                        },
+                    )
+                ],
+            ),
+        },
+        description="Вход или регистрация пользователя. Отправляет код на указанный номер телефона.",
+    )
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -52,13 +131,14 @@ class CodeVerificationView(APIView):
                 {"error": "Неверный или не существующий номер телефона."}
             )
         if user.confirmation_code != confirmation_code:
-            return Response({"error": "Неверный код."})
+            return Response({"error": "Неверный код верификации."})
         token, _ = Token.objects.get_or_create(user=user)
         user.confirmation_code = None
         user.save(update_fields=["confirmation_code"])
         return Response({"token": token.key})
 
 
+@extend_schema(tags=["Пользователи"])
 class UserViewSet(ModelViewSet):
     """Представление для пользователей."""
 
