ac087d588d0010be2e2eb777c637d22402907672
bissaliev <bissaliev21@gmail.com>
2024-12-01 18:35:03 +0400

    Оформление описание проекта

--- referralapp/referralapp/settings.py
+++ referralapp/referralapp/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
-print(SECRET_KEY)
 
 DEBUG = bool(os.getenv("DEBUG", False))
 
@@ -66,15 +65,7 @@ WSGI_APPLICATION = "referralapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if os.getenv("DATABASE") == "sqlite":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
-else:
+if os.getenv("DATABASE") == "postgres":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
@@ -83,6 +74,14 @@ else:
             "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
             "HOST": os.getenv("DB_HOST", ""),
             "PORT": os.getenv("DB_PORT", 5432),
+        }
+    }
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 
