fab2f9473796add25fe9e2ee8f2f604fa51a1b13
bissaliev <bissaliev21@gmail.com>
2024-12-02 21:07:13 +0400

    Рефакторинг users/models

--- referralapp/users/models.py
+++ referralapp/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.exceptions import ValidationError
 from django.db import models
+
 from users.utils import generate_invite_code
 from users.validators import validate_invite_code, validate_phone_number
 
